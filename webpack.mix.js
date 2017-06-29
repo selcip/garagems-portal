8372d1263d11e63673d19d5a46a37449766c24eb
nyeedz <chr.alex@hotmail.com>
2017-06-29 17:22:22 -0300

    wfasdasf

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css').
-   browserSync({
-       proxy: 'maple.dev'
-   });
+    .sass('resources/assets/sass/app.scss', 'public/css').options({ processCssUrls: false })
+    .browserSync({
+      proxy: 'localhost:8000'
+    });
